7ab5ff2b523d3e87875f98814dd68867780f0f96
Pyxyll <pyxyllv@gmail.com>
2023-10-16 00:09:32 +0100

    fix mailer

--- api/contact.mjs
+++ api/contact.mjs
@@ -1,37 +1,32 @@
+const nodemailer = require('nodemailer');
+
 module.exports = async (req, res) => {
+    const { name, email, domain, subject, message, file } = req.body;
 
-    const nodemailer = require('nodemailer');
+    // Create a transporter using nodemailer (for this example, we'll use Gmail)
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.EMAIL_USER, // your Gmail address
+            pass: process.env.EMAIL_PASS  // your Gmail password
+        }
+    });
 
-    module.exports = async (req, res) => {
-        const { name, email, domain, subject, message, file } = req.body;
-
-        // Create a transporter using nodemailer (for this example, we'll use Gmail)
-        let transporter = nodemailer.createTransport({
-            service: 'gmail',
-            auth: {
-                user: process.env.EMAIL_USER, // your Gmail address
-                pass: process.env.EMAIL_PASS  // your Gmail password
-            }
-        });
-
-        // Mail options
-        let mailOptions = {
-            from: process.env.EMAIL_USER, // sender address
-            to: 'your-email@example.com', // list of receivers
-            subject: 'New Contact Form Submission', // Subject line
-            text: `Name: ${name}\nEmail: ${email}\nDomain: ${domain}\nSubject: ${subject}\nMessage: ${message}` // plaintext body
-        };
-
-        // Send the email
-        transporter.sendMail(mailOptions, (error, info) => {
-            if (error) {
-                console.log(error);
-                res.status(500).send({ status: 'Error sending email' });
-            } else {
-                res.status(200).send({ status: 'Form submitted successfully' });
-            }
-        });
+    // Mail options
+    let mailOptions = {
+        from: process.env.EMAIL_USER, // sender address
+        to: 'your-email@example.com', // list of receivers
+        subject: 'New Contact Form Submission', // Subject line
+        text: `Name: ${name}\nEmail: ${email}\nDomain: ${domain}\nSubject: ${subject}\nMessage: ${message}` // plaintext body
     };
 
-    res.status(200).send({ status: 'Form submitted successfully' });
+    // Send the email
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.log(error);
+            res.status(500).send({ status: 'Error sending email' });
+        } else {
+            res.status(200).send({ status: 'Form submitted successfully' });
+        }
+    });
 };
